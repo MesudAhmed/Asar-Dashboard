b1ec807e790367292d51bb30a06b4f4649d69dd8
modar-alsakher <modarsk108@gmail.com>
2025-04-30 11:18:27 +0300

    Create OTP Page

--- pages/admin/auth/otp.vue
+++ pages/admin/auth/otp.vue
@@ -2,21 +2,18 @@
 import { useLoginUser } from '@@/queries/auth/user'
 import { loginSchema } from '~/schema/loginUser.schema'
 
-const userForm = ref({
-  email: '',
-  password: '',
+const otpForm = ref({
+  otp: ['', '', '', ''],
+})
+const otpString = computed(() => otpForm.value.otp.join(''))
+const formIsValid = computed(() => {
+  return otpForm.value.otp.some(digit => !digit)
 })
 
 const isLoading = ref(false)
 const errorStore = useErrorStore()
 const toast = useToast()
 const form = ref()
-
-const formIsValid = computed(() => {
-  if (!form.value) return true
-
-  return form.value.errors && Object.keys(form.value.errors).length > 0
-})
 </script>
 
 <template>
@@ -28,33 +25,28 @@ const formIsValid = computed(() => {
             Team Manager Login
           </p>
           <p class="text-[#868686] pt-3">
-            enter your Email to send OTP
+            enter your OTP that we Send in your Email
           </p>
         </div>
         <UForm
           ref="form"
-          :state="userForm"
           action="#"
           :schema="loginSchema"
-          class="space-y-4"
+          class="space-y-4 text-center"
+          @submit.prevent="navigateTo('/admin/auth/forgetPassword')"
         >
-          <UFormField
-            class="text-primary"
-            label="Email"
-            name="email"
-          >
-            <UInput
-              v-model="userForm.email"
-              class="border-0 rounded-lg bg-white text-black block"
-              size="xl"
-              variant="subtle"
-            />
-          </UFormField>
+          <UPinInput
+            v-model="otpForm.otp"
+            otp
+            color="primary"
+            size="xl"
+            :length="6"
+          />
           <UButton
-            type="button"
+            type="submit"
             block
             class="w-full sm:mt-3 disabled:bg-primary text-white bg-primary hover:bg-blue-950 rounded-full h-12 py-2.5 font-bold text-center"
-            @click="navigateTo('/admin/auth/otp')"
+            @click="navigateTo('../dashboard')"
           >
             <span class="font-bold text-lg">Sign in</span>
           </UButton>
