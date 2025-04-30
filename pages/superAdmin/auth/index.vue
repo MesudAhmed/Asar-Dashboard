5cab3b4d39ac311af51cee2a8d12f5a3e525842d
MesudAhmed <mesudahmed403@gmail.com>
2025-04-30 09:32:40 +0300

    create super admin login page

---
+++ pages/superAdmin/auth/index.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { useLoginAdmin } from '@@/queries/auth/admin'
+import type { LoginResponse } from '~/models/loginResponseModel'
+import type { UserModel } from '~/models/userModel'
+
+const router = useRouter()
+
+const userLoginFunction = async (userData: UserModel) => {
+  const { data, status } = await useLoginAdmin(userData)
+  if (status.value === 'success') {
+    const loginData = data.value as LoginResponse
+    router.push('../admin/dashboard')
+
+    return { success: true, role: loginData.role, token: loginData.token }
+  } else {
+    return { success: false }
+  }
+}
+</script>
+
+<template>
+  <div class="w-full h-dvh flex flex-col md:flex-row">
+    <Auth :login-function="userLoginFunction">
+      <p class="text-lg md:text-2xl font-bold text-black mt-16">
+        Super Admin Login
+      </p>
+      <p class="text-[#868686] pt-3">
+        Enter your Email and Password
+      </p>
+      <template #button>
+        <NuxtLink
+          to="/auth/user/register"
+          class="font-bold cursor-pointer text-primary hover:underline"
+        >Sign in</NuxtLink>
+      </template>
+    </Auth>
+    <div class="w-1/2 bg-primary md:grid hidden h-screen place-items-center">
+      <img
+        src="/SuperAminLogin.png"
+        loading="lazy"
+        class="w-full md:w-xl"
+      >
+    </div>
+  </div>
+</template>
