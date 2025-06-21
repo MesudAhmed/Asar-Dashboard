b20f3df0b22fd0b11cf1dc7492f246e83f2e7fbf
MesudAhmed <mesudahmed403@gmail.com>
2025-06-21 13:33:14 +0300

    Fix Eslint Error in Auth Page

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -24,7 +24,7 @@ const userLoginFunction = async (userData: UserModel) => {
       <p class="text-[#868686] pt-3">
         Enter your Email and Password
       </p>
-      <template #button> 
+      <template #button>
         <NuxtLink class="font-bold cursor-pointer text-primary hover:underline">
           Sign in
         </NuxtLink>
