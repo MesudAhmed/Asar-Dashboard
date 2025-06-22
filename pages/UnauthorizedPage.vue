a73de2ec7faa0a5cf5ad9d2107a886e622a11a6f
modar-alsakher <modarsk108@gmail.com>
2025-06-22 14:47:58 +0300

    Create Unauthorized Page

---
+++ pages/UnauthorizedPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <UError
+    :error="{
+      statusCode: 401,
+      statusMessage: 'Unauthorized',
+      message: 'you donot login yet.',
+    }"
+    redirect="/auth"
+  />
+</template>
